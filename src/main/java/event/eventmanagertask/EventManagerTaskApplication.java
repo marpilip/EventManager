ebf47b2fe3a2097058273d53d0c0a8d2c74bda25
marpilip <mar.pilip@gmail.com>
2025-08-01 11:00:46 +0700

    Начат проект EventManager

---
+++ src/main/java/event/eventmanagertask/EventManagerTaskApplication.java
@@ -0,0 +1,13 @@
+package event.eventmanagertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/event/eventmanagertask/EventManagerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package event.eventmanagertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
